7cbf443665a9280db7749d565f7138b2192e5062
Dylan Stanfield <dylanstanfield@live.com>
2022-11-05 12:35:06 -0400

    add lazy loading test
    add lazy loading test

---
+++ apps/flows/src/components/CodeSplitTest.tsx
@@ -0,0 +1,11 @@
+import { faker } from '@faker-js/faker';
+
+const CodeSplitTest = () => {
+  console.log(faker)
+
+  return (
+    <div>loaded</div>
+  )
+}
+
+export default CodeSplitTest
--- apps/flows/src/pages/HelloWorldPage.tsx
+++ apps/flows/src/pages/HelloWorldPage.tsx
@@ -1,4 +1,7 @@
-import { styled, Typography } from "@mui/material"
+import { styled, Typography } from '@mui/material'
+import { Suspense, lazy } from 'react';
+
+const CodeSplitTest = lazy(() => import('../components/CodeSplitTest'));
 
 const Text = styled(Typography)`
   color: red;
@@ -8,6 +11,10 @@ export const HelloWorldPage = () => {
   return (
     <Text variant="h1">
       Hello World!
+
+      <Suspense fallback={<div>Loading...</div>}>
+        <CodeSplitTest />
+      </Suspense>
     </Text>
   )
 }
