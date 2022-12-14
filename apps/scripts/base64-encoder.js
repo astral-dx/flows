270ced37103326c327d74ce4128d2690914a8b5e
Dylan Stanfield <dylanstanfield@live.com>
2022-12-13 21:33:57 -0500

    update scripts

--- apps/scripts/base64-encoder.js
+++ apps/scripts/base64-encoder.js
@@ -2,7 +2,7 @@ var fs = require('fs')
 var path = require('path')
 
 function main() {
-    var bitmap = fs.readFileSync('./images/lendflow-goldenapi6.png')
+    var bitmap = fs.readFileSync('./images/lendflow-goldapi19.png')
     console.log(Buffer.from(bitmap).toString('base64'))
 }
 
