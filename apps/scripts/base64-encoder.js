39cd55e9085950ce0de77d9a14554332e38fc4a4
Dylan Stanfield <dylanstanfield@live.com>
2022-12-15 02:11:03 -0500

    add image changes

--- apps/scripts/base64-encoder.js
+++ apps/scripts/base64-encoder.js
@@ -2,8 +2,8 @@ var fs = require('fs')
 var path = require('path')
 
 function main() {
-    var bitmap = fs.readFileSync('./images/lendflow-goldapi19.png')
-    console.log(Buffer.from(bitmap).toString('base64'))
+    var bitmap = fs.readFileSync('./images/cacheflow-1.png')
+    console.log('data:image/png;base64,' + Buffer.from(bitmap).toString('base64'))
 }
 
 main()
